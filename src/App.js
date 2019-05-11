21e46c1335bf5ad9ff24dddf305df026164af9b9
TATY <tatyccsilva@gmail.com>
2019-05-11 12:39:41 +0200

    Event handling and local state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
+// import { createStore } from 'redux';
+
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+}
+
+// const reducer = (state = 'red', action) => {
+//   return state
+// }
+
+// const store = createStore(reducer)
 
 class App extends Component {
+  state = {
+    value: ''
+  }
 
+  updateSelection = (event) => {
+    console.log(event.target.value, event);
+    this.setState({selected: event.target.value})
+  }
 
   render() {
+    // console.log(store.getState());
+
     return (
       <div className="App">
-        <header className="App-header">
-
-        </header>
+        <h3>Computer models</h3>
+        <select value={this.state.selected} onChange={this.updateSelection}>
+          <option value="">-- pick a model --</option>
+          {Object.entries(data).map(computer =>
+            <option key={computer[0]} value={computer[0]}>
+              {computer[0]} ({computer[1].year})
+            </option>
+          )}
+        </select>
       </div>
     );
   }
